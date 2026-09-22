041ec491abfb63b4e06db2fdcf5f3493d78da17a
gseptarisa-ctrl <gseptarisa@gmail.com>
2026-09-22 10:49:49 +0700

    membuat kondisi if, else if dan else pada nilai dengan menggunakan int

---
+++ Nomor1.py
@@ -0,0 +1,11 @@
+nilai = int (input("Masukkan nilai kamu: "))
+if nilai >= 90:
+    print("Excelent Performance")
+elif nilai >= 80:
+    print("Very Good Performance")
+elif nilai >= 70:
+    print("Good Performance")
+elif nilai >= 60:
+    print("Average Performance")
+else:
+    print("Poor Performance")
